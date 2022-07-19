3648867b378b536dfde2a2fad4e3e2504a4daaf4
GaliusXhantus <Xhantusgalius@gmail.com>
2022-07-19 16:00:54 +0000

    Implementando botão de recomeçar.

--- script/index-script.js
+++ script/index-script.js
@@ -1,4 +1,5 @@
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('main table tr td button');
+const resetar = document.querrySelector('main button');
 
 let jogador = 'x';
 
@@ -9,11 +10,17 @@ function mudarJogador(){
     else {return jogador = 'x';}
 }
 
+function resetarTela(event){
+    resetar.addEventListener('click', ()=>{
+        return(event.innerHTML = '');
+    })
+}
 
 function jogo(){
     for (const button of buttons){
         button.addEventListener('click', ()=> {
             button.innerHTML = jogador;
+            resetarTela(button);
             mudarJogador();
         });
     };
