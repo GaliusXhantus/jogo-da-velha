db49755df265dff914a2805d7b374754b9aa6d58
GaliusXhantus <Xhantusgalius@gmail.com>
2022-07-23 20:21:09 +0000

    Inpedindo que o jogador mude as casas já marcadas.

--- script/index-script.js
+++ script/index-script.js
@@ -1,21 +1,35 @@
-//import { arrayVencedor } from "./array-vencedor";
+// import { arrayVencedor } from "./array-vencedor";
 
 
 const buttons = document.querySelectorAll('main table tr td button');
 const resetar = document.querySelector('main > button');
-const span = document.querySelector('main p span')
+const span = document.querySelector('main p span');
 
 let jogador = 'x';
 
-function mostrarJogador(){
-    return span.innerHTML = jogador;
+function mostrarJogador(event){
+
+    if(event.innerHTML === ''){
+        event.innerHTML = jogador;
+        mudarJogador();
+    }
+    else{
+        return false
+    }
+}
+
+function jogando(){
+
+        return span.innerHTML = jogador;        
 }
 
 function mudarJogador(){
-    if (jogador === 'x'){
+    
+    if (jogador === 'x'){ 
         return jogador = 'o';
     }
     else {return jogador = 'x';}
+    
 }
 
 function resetarTela(event){
@@ -24,18 +38,23 @@ function resetarTela(event){
     })
 }
 
-//function vencedor(posicao){
-//    let arrayJogo = [buttons[posicao]];
+//function vencedor(){
+//
+//    if (buttons[1] === buttons[2] && buttons[2] === buttons[3]){
+//        return console.log('hello')
+//    }
+//    else{
+//        return false
+//    }
 //
 //}
 
 function jogo(){
     for (const button of buttons){
         button.addEventListener('click', ()=> {
-            button.innerHTML = jogador;
-            mudarJogador();
-            mostrarJogador();
-//            vencedor(button);
+            mostrarJogador(button);
+            jogando();
+  //          vencedor();
             resetarTela(button);
         });
     };
